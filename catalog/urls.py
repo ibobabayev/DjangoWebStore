f6922adad11cccc0feab6071c9f490403eea297b
user <ibrahim42babayev@gmail.com>
2024-02-16 19:22:48 +0300

    #1 Try

---
+++ catalog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from catalog.views import home,contact
+
+urlpatterns = [
+    path('', home),
+    path('contact/', contact)
+]
---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'Имя пользователя: {name}, email: {email} , Отзыв: {message}')
+    return render(request,'contact.html')
