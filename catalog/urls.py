545b2ff03d106e00bce5d411f329abac0f02fd9e
user <ibrahim42babayev@gmail.com>
2024-02-25 00:17:30 +0300

    #HW_20.1 Final try

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from catalog.views import home,contact
+from catalog.views import home,contact,latest
 
 urlpatterns = [
     path('', home),
-    path('contact/', contact)
+    path('contact/', contact),
+    path('latest/', latest)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,14 +3,21 @@ from catalog.models import Product,Contact
 
 # Create your views here.
 def home(request):
-    print(Product.objects.order_by('price')[:5])
     return render(request,'home.html')
 
+def latest(request):
+    product_list = Product.objects.order_by('price')[:5]
+    products = {'filtred_list': product_list}
+    return render(request, 'latest.html', products)
+# def contact(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(f'Имя пользователя: {name}, email: {email} , номер: {phone} , Отзыв: {message}')
+#     return render(request,'contact.html',{'contact_book':Contact.objects.all()})
 def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'Имя пользователя: {name}, email: {email} , номер: {phone} , Отзыв: {message}')
-    return render(request,'contact.html',{'contact_book':Contact.objects.all()})
+    contact_info = Contact.objects.all()
+    contact_info = {'contact_book':contact_info}
+    return render(request,'contact.html',contact_info)
